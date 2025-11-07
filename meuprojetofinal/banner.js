cec3786526e89398f16c1483cc0483a88e42869e
BERNARDO ANDRADE DA LUZ <05737004007@senacrs.edu.br>
2025-11-07 12:21:46 -0300

    Teste de Envio De Arquivo

---
+++ meuprojetofinal/banner.js
@@ -0,0 +1,30 @@
+        var imagens = [
+            "1.jpg",
+            "2.jpg",
+            "3.png",
+        ];
+ 
+        var indice = 0;
+ 
+        var banner = document.getElementById('banner');
+ 
+        function monstrarImagens(){
+            banner.src = imagens[indice];
+        }
+ 
+        function Ir(){
+            //incrementar o indice
+            indice++;
+            if(indice >= imagens.length){
+                indice = 0;
+            }
+            monstrarImagens();
+ 
+        }
+        function Voltar(){
+            indice--;
+            if(indice < 0){
+                 indice = imagens.length - 1;  
+            }
+            monstrarImagens();
+        }
---
+++ projeto/js/enviacontato.php
@@ -0,0 +1,30 @@
+<?php
+
+ini_set ( 'default_charset' , 'utf8' ) ;
+if ( isset ( $_POST[ 'enviar' ] ) ) {
+
+       $nome = htmlspecialchars(strip_tags($_POST[ 'nome' ])) ;
+       $email = strip_tags(trim($_POST[ 'email' ])) ;
+       $assunto = htmlspecialchars(strip_tags($_POST[ 'assunto' ])) ;
+       $mensagem = htmlspecialchars(strip_tags($_POST[ 'mensagem' ])) ;
+
+
+       if ( ( ! empty ( $_POST[ 'email' ] ))) {
+
+              $email = $_POST[ 'email' ] ;
+              if ( filter_var ( $email , FILTER_VALIDATE_EMAIL ) ) {
+                     require 'Email.php' ;
+                     $corpo = 'WQI Consultoria  Contato- '  . " Código: ". $nome = $_POST[ 'nome' ] . " Email:  " . $email = $_POST[ 'email' ] . " Assunto: " . $assunto = $_POST[ 'assunto' ] . " Mensagem ". $mensagem = $_POST[ 'mensagem' ]  ;
+                     $assunto = 'Site - WQI Consultoria - ' ;
+                     $razao = $_POST[ 'nome' ] ;
+                     $email = Email::enviaNovoEmail ( $corpo , $assunto , $email , $razao ) ;
+              }
+       }
+       else {
+              echo "<script>alert('Ivalido!');top.location.href='contatos.html';</script>" ;
+       }
+}
+unset ( $_POST );
+
+
+
