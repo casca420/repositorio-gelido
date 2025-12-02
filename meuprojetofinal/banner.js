0d5c167331bacc220a50e9fbd9f0cab828369b4c
BERNARDO ANDRADE DA LUZ <05737004007@senacrs.edu.br>
2025-12-02 11:40:04 -0300

    Teste de Envio De Arquivo

--- meuprojetofinal/banner.js
+++ meuprojetofinal/banner.js
@@ -1,30 +1,50 @@
-        var imagens = [
-            "banner 1.jpg",
-            "banner 2.jpg",
-            "banner 3.webp",
-        ];
- 
-        var indice = 0;
- 
-        var banner = document.getElementById('banner');
- 
-        function monstrarImagens(){
-            banner.src = imagens[indice];
-        }
- 
-        function Ir(){
-            //incrementar o indice
-            indice++;
-            if(indice >= imagens.length){
-                indice = 0;
-            }
-            monstrarImagens();
- 
-        }
-        function Voltar(){
-            indice--;
-            if(indice < 0){
-                 indice = imagens.length - 1;  
-            }
-            monstrarImagens();
-        }
+var imagens = [
+    "banner 1.jpg",
+    "banner 2.jpg",
+    "banner 3.webp",
+];
+
+var indice = 0;
+
+var banner = document.getElementById('banner');
+
+function monstrarImagens() {
+    banner.src = imagens[indice];
+}
+
+function Ir() {
+    indice++;
+    if (indice >= imagens.length) {
+        indice = 0;
+    }
+    monstrarImagens();
+
+    reiniciarAutoSlide();
+}
+
+function Voltar() {
+    indice--;
+    if (indice < 0) {
+        indice = imagens.length - 1;
+    }
+    monstrarImagens();
+
+    reiniciarAutoSlide();
+}
+
+
+var tempo = 4000; 
+var slideInterval; 
+
+function autoSlide() {
+    slideInterval = setInterval(Ir, tempo);
+}
+
+function reiniciarAutoSlide() {
+    clearInterval(slideInterval); 
+    autoSlide();
+}
+
+
+monstrarImagens();
+autoSlide();
